0b28d98bd2c483071a1d76d50dad7161d5800ad2
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-07-01 23:40:01 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const date = new Date().getFullYear();
+document.getElementById("currentYear").textContent = date;
